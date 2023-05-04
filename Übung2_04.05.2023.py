9041b4dbc00dde762ddd79edf369bb32ca23ffcb
OliverFett <132549236+OliverFett@users.noreply.github.com>
2023-05-04 11:39:24 +0200

    Operatoren_04.05.2023
    If und While

---
+++ Übung2_04.05.2023.py
@@ -0,0 +1,23 @@
+x = int(input("Zahl x:"))
+y= int(input("Zahl y:"))
+#if [Bedinung] :
+    #Block1
+#else:
+    #Block2
+#if y == 0:
+    #print("Zahl y kann darf nicht Null sein! Gib nochmal Zahl y ein!")
+    #y = int(input("Zahl y:"))
+    
+
+#while [Bedinung] :
+    #Anweisungen...
+    
+#x = 0
+#while x <= 10:
+    #print(x)
+    #x= x+1
+
+while y <= 0:
+    print("Zahl y kann darf nicht Null sein! Gib nochmal Zahl y ein!")
+    y = int(input("Zahl y:"))
+    
